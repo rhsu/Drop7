a333c62c7cdb433ef5939eb0890190ea2b8e8569
robert <rhsu123@gmail.com>
2013-02-26 17:07:35 -0500

    Suggestions from Code Review

--- Drop7/src/GameObjects/Enhanced/BoardEnhanced.java
+++ Drop7/src/GameObjects/Enhanced/BoardEnhanced.java
@@ -21,14 +21,34 @@ public class BoardEnhanced extends Board
 		}
 	}
 	
+	private BoardEnhanced(boolean b)
+	{
+		board = new PieceEnhanced[7][7];
+		int ctr = 0;
+		for (int i = 0; i < 7; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{
+				board[i][j] = (i < 6) ? new PieceEnhanced(Piece.Type.EMPTY, i, j) : new PieceEnhanced(Piece.Type.NEW, i, j, ++ctr);
+			}
+		}
+	}
+	
 	public PieceEnhanced PieceAt(int i, int j)
 	{
 		return (PieceEnhanced) board[i][j];
 	}
 	
+	public PieceEnhanced GetLeftPiece(int i, int j)
+	{		
+		return (i == 0 ? null : PieceAt(i, j-1));
+	}
+	
 	public static void main(String[] args)
 	{
-		BoardEnhanced b  = new BoardEnhanced();
-		b.PieceAt(0, 0).GetLeftPiece(b);
+		BoardEnhanced b = new BoardEnhanced(false);
+		System.out.println(b);
+		Piece p = b.GetLeftPiece(6, 3);
+		System.out.println(p);
 	}
 }
