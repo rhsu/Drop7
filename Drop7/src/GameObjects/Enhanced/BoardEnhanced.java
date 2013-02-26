2781b6beb5256d25284084574b6b37ceb8bcdf26
robert <rhsu123@gmail.com>
2013-02-26 15:47:08 -0500

    Comitting for review
    Posting question on code review. Not sure if should proceed

--- Drop7/src/GameObjects/Enhanced/BoardEnhanced.java
+++ Drop7/src/GameObjects/Enhanced/BoardEnhanced.java
@@ -1,6 +1,7 @@
 package GameObjects.Enhanced;
 
 import GameObjects.Board;
+import GameObjects.Piece;
 
 /**
  *
@@ -10,11 +11,24 @@ public class BoardEnhanced extends Board
 {
 	public BoardEnhanced()
 	{
-		super();
+		board = new PieceEnhanced[7][7];
+		for (int i = 0; i < 7; i++)
+		{
+			for (int j = 0; j < 7; j++)
+			{
+				board[i][j] = (i < 6) ? new PieceEnhanced(Piece.Type.EMPTY, i, j) : new PieceEnhanced(Piece.Type.NEW, i, j);
+			}
+		}
 	}
 	
 	public PieceEnhanced PieceAt(int i, int j)
 	{
 		return (PieceEnhanced) board[i][j];
+	}
+	
+	public static void main(String[] args)
+	{
+		BoardEnhanced b  = new BoardEnhanced();
+		b.PieceAt(0, 0).GetLeftPiece(b);
 	}
 }
--- Drop7/src/GameObjects/Enhanced/PieceEnhanced.java
+++ Drop7/src/GameObjects/Enhanced/PieceEnhanced.java
@@ -8,13 +8,84 @@ import GameObjects.Piece;
  */
 public class PieceEnhanced extends Piece
 {	
+	protected int horizontal;
+	protected int vertical;
+	
 	public PieceEnhanced(Type type)
 	{
 		super(type);
 	}
 	
-	public PieceEnhanced(Type type, int value)
+	public PieceEnhanced(Type type, int horizontal, int vertical)
+	{
+		super(type);
+		this.horizontal = horizontal;
+		this.vertical = vertical;
+	}
+	
+	public PieceEnhanced(Type type, int horizontal, int vertical, int value)
 	{
 		super(type, value);
+		this.horizontal = horizontal;
+		this.vertical = vertical;
+		this.value = value;
+	}
+	
+	public int GetHorizontal()
+	{
+		return this.horizontal;
+	}
+	
+	public int GetVertical()
+	{
+		return this.vertical;
+	}
+	
+	public PieceEnhanced GetLeftPiece(BoardEnhanced board)
+	{
+		if(this.horizontal == 0) 
+		{
+			return null;
+		}
+		else 
+		{
+			return board.PieceAt(this.horizontal, this.vertical - 1);
+		}
+	}
+	
+	public PieceEnhanced GetRightPiece()
+	{
+		if(this.vertical == 0) 
+		{
+			return null;
+		}
+		else 
+		{
+			return null;
+		}
+	}
+	
+	public PieceEnhanced GetUpPiece()
+	{
+		if(this.vertical == 0) 
+		{
+			return null;
+		}
+		else 
+		{
+			return null;
+		}
+	}
+	
+	public PieceEnhanced GetDownPiece()
+	{
+		if(this.vertical == 0) 
+		{
+			return null;
+		}
+		else 
+		{
+			return null;
+		}
 	}
 }
