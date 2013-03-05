9700acd0b4a827b60b4ac9bc24db25a4ce369fc1
robert <rhsu123@gmail.com>
2013-03-05 12:55:17 -0500

    Created Editor fold for retreival functions

--- Drop7/src/GameObjects/Board.java
+++ Drop7/src/GameObjects/Board.java
@@ -3,48 +3,9 @@ package GameObjects;
 import java.util.ArrayList;
 
 public class Board extends BoardBase
-{	
-	/**
-	 * Constructs a board for playing the Drop7 game.
-	 */
-	public Board()
-	{
-		super();
-	}
-		
-	/**
-	 * Given a position, this function will create a new piece and update the piece
-	 * @param position The column number to insert the piece. Do not use with array indexes
-	 * @return True if the insert was successful, else returns false
-	 * @throws IllegalArgumentException If an incorrect position or value is given+
-	 */
-	public boolean insert(int position, int value)
-	{
-		if((position < 1) || (position > 7) || (value > 8))
-		{
-			throw new IllegalArgumentException();
-		}
-		
-		//subtracting one to calibrate the position to work with array indexes
-		position--;
-		
-		for (int index = 6; index >= 0; index--)
-		{
-			Piece current = pieceAt(index, position);
-			if(current.getType() == Piece.Type.EMPTY)
-			{				
-				current.setType(value == 8 ? Piece.Type.MYSTERY1 : Piece.Type.SET);	
-				current.setValue(value);
-                                //int h = getHorizontalAdjacent(current); 
-                                //int v = getColumnAdjacent(current);
-                                //System.out.println("h is: " + h + " and v is : "+ v);
-				return true;
-			}
-		}
-		return false;
-	}
-        
-        /**
+{
+    	//<editor-fold defaultstate="collapsed" desc="RETRIEVAL FUNCTIONS">
+                /**
          * calculates the number of adjacent pieces in the same (horizontal) row as a
          * given parameter piece (Including itself: e.g. A piece with no pieces next to it
          * will return a value of 1)
@@ -183,10 +144,51 @@ public class Board extends BoardBase
             }
             return list;
         }
-
+        
+        //</editor-fold>
+    
+	/**
+	 * Constructs a board for playing the Drop7 game.
+	 */
+	public Board()
+	{
+		super();
+	}
+		
+	/**
+	 * Given a position, this function will create a new piece and update the piece
+	 * @param position The column number to insert the piece. Do not use with array indexes
+	 * @return True if the insert was successful, else returns false
+	 * @throws IllegalArgumentException If an incorrect position or value is given+
+	 */
+	public boolean insert(int position, int value)
+	{
+		if((position < 1) || (position > 7) || (value > 8))
+		{
+			throw new IllegalArgumentException();
+		}
+		
+		//subtracting one to calibrate the position to work with array indexes
+		position--;
+		
+		for (int index = 6; index >= 0; index--)
+		{
+			Piece current = pieceAt(index, position);
+			if(current.getType() == Piece.Type.EMPTY)
+			{				
+				current.setType(value == 8 ? Piece.Type.MYSTERY1 : Piece.Type.SET);	
+				current.setValue(value);
+                                //int h = getHorizontalAdjacent(current); 
+                                //int v = getColumnAdjacent(current);
+                                //System.out.println("h is: " + h + " and v is : "+ v);
+				return true;
+			}
+		}
+		return false;
+	}
+        
 	public static void main(String[] args)
 	{		
 
 	}
 }
-
