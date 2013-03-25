89707a65fac0d78d15e11d5f7f1679e1ed196f7a
robert <rhsu123@gmail.com>
2013-03-25 15:29:34 -0400

    Reorganized board and board base

--- Drop7/src/GameObjects/BoardBase.java
+++ Drop7/src/GameObjects/BoardBase.java
@@ -1,6 +1,7 @@
 package GameObjects;
 
 import Utilities.PublicFunctions;
+import java.util.ArrayList;
 
 public class BoardBase 
 {
@@ -94,6 +95,149 @@ public class BoardBase
 		return pieceAt(p.getHorizontal() + 1, p.getVertical());
 	}
 	
+	//</editor-fold>
+	
+	//<editor-fold defaultstate="collapsed" desc="RETRIEVAL FUNCTIONS">
+	/**
+	* calculates the number of adjacent pieces in the same (horizontal) row as a
+	* given parameter piece (Including itself: e.g. A piece with no pieces next to it
+	* will return a value of 1)
+	* @param p the parameter piece to perform the calculation on
+	* @return the number of pieces that are in the same row as the parameter piece.
+	*/
+	public int getRowAdjacent(Piece p)
+	{
+		if((p == null) || (p.getType() == Piece.Type.EMPTY))
+		{
+			return 0;
+		}
+                
+		Piece current = p;
+		int numAdjacent = 0;
+		while((getLeftPiece(current) != null) && (getLeftPiece(current).getType() != Piece.Type.EMPTY))
+		{
+			numAdjacent++;
+			current = getLeftPiece(current);
+		}
+                
+		current = p;
+                
+		while((getRightPiece(current) != null) && (getRightPiece(current).getType() != Piece.Type.EMPTY))
+		{
+			numAdjacent++;
+			current = getRightPiece(current);
+		}
+
+		numAdjacent++; //increment to include self;
+                
+		return numAdjacent;     
+	}
+	
+	/**
+	* calculates the number of adjacent pieces in the same (vertical) column as a
+	* given parameter piece (Including itself: e.g. A piece with no pieces next to it
+	* will return a value of 1)
+	* @param p the parameter piece to perform the calculation on
+	* @return the number of pieces that are in the same column as the parameter piece.
+	*/
+	public int getColumnAdjacent(Piece p)
+	{
+		if((p == null) || (p.getType() == Piece.Type.EMPTY))
+		{
+			return 0;
+		}
+            
+		Piece current = p;
+		int numAdjacent = 0;
+
+		//get the pieces below
+		while((getUpPiece(current) != null) && (getUpPiece(current).getType() != Piece.Type.EMPTY))
+		{
+			numAdjacent++;
+			current = getUpPiece(current);
+		}
+                
+		current = p; //reset current: need to count the down pieces
+                
+		//get the pieces above
+		while((getDownPiece(current) != null) && (getDownPiece(current).getType() != Piece.Type.EMPTY))
+		{
+			numAdjacent++;
+			current = getDownPiece(current);
+		}
+                
+		numAdjacent++; //include self
+                
+		return numAdjacent;
+	}
+        
+	/**
+	* @param p The piece to perform the method on
+	* @return A list of pieces that are in the same row as the parameter piece
+	*/
+	public ArrayList<Piece> getAllInRow(Piece p)
+	{
+		if (p == null)
+		{
+			throw new NullPointerException();
+		}
+		int row = p.getHorizontal();
+		ArrayList<Piece> list = new ArrayList<>();
+		for(int j = 0; j < 7; j++)
+		{
+			Piece temp = pieceAt(row, j);
+			if((temp != null) && (temp.getType() != Piece.Type.EMPTY))
+			{
+				list.add(temp);
+			}
+		}
+		return list;
+	}
+        
+	/**
+	* @param p The piece to perform the method on
+	* @return A list of pieces that are in the same column as the parameter piece
+	*/
+	public ArrayList<Piece> getAllInColumn(Piece p)
+	{
+		if (p == null)
+		{
+			throw new NullPointerException();
+		}
+		int column = p.getVertical();
+            ArrayList<Piece> list = new ArrayList<>();
+            for(int i = 0; i < 7; i++)
+            {
+                //System.out.println(pieceAt(row, i));
+                Piece temp = pieceAt(i, column);
+                if((temp != null) && (temp.getType() != Piece.Type.EMPTY))
+                {
+                    list.add(temp);
+                }
+            }
+            return list;
+        }
+        
+	/**
+	* @return A list of pieces that are marked as remove
+	*/
+	public ArrayList<Piece> getAllRemove()
+	{
+		ArrayList<Piece> list = new ArrayList<>();
+		for(int i = 0; i < 7; i++)
+		{
+			for(int j = 0; j < 7; j++)
+			{
+				Piece p = pieceAt(i, j);
+				if(p.getRemove())
+				{
+					list.add(p);
+				}
+			}
+		}
+		return list;
+	}
+        
 	//</editor-fold>
 	
 	/**
