8746e489b8ea275b46741da103850db73906459f
robert <rhsu123@gmail.com>
2013-04-05 16:45:15 -0400

    #40 Committing for backup

--- Drop7/src/abstraction/AbstractPiece.java
+++ Drop7/src/abstraction/AbstractPiece.java
@@ -1,6 +1,32 @@
 package abstraction;
 
-public abstract class AbstractPiece <T>
+public abstract class AbstractPiece<T>
 {
+	private int horizontal;
+	private int vertical;
+	private T piece;
 	
+	public AbstractPiece(T t, int i, int j)
+	{
+		piece = (T) new Object();
+		this.horizontal = i;
+		this.vertical = i;
+	}
+		
+	/**
+	* @return the horizontal
+	*/
+	public int getHorizontal()
+	{
+		return this.horizontal;
+	}
+	
+	/**
+	 * 
+	 * @return the vertical 
+	 */
+	public int getVertical()
+	{
+		return this.vertical;
+	}
 }
