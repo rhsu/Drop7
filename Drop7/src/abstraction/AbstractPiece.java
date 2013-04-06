a6febd58144711d0e2b5a9528cab2cf1a514a39e
Robert Hsu <robert@robert-Satellite-C655.(none)>
2013-04-06 09:55:49 -0400

    #40 Added Editor Fold

--- Drop7/src/abstraction/AbstractPiece.java
+++ Drop7/src/abstraction/AbstractPiece.java
@@ -2,17 +2,13 @@ package abstraction;
 
 public abstract class AbstractPiece<T>
 {
+	//<editor-fold  defaultstate="collpased" desc="MEMBER VARIABLES">
 	private int horizontal;
 	private int vertical;
 	protected T piece;
+	//</editor-fold>
 	
-	public AbstractPiece(T t,int i, int j)
-	{
-		//piece = (T) new Object(); //commenting out because method is unsafe
-		this.horizontal = i;
-		this.vertical = i;
-	}
-		
+	//<editor-fold defaultstate="collapsed" desc="ACCESSORS">
 	/**
 	* @return the horizontal
 	*/
@@ -29,4 +25,13 @@ public abstract class AbstractPiece<T>
 	{
 		return this.vertical;
 	}
+	//</editor-fold>
+	
+	public AbstractPiece(T t,int i, int j)
+	{
+		//piece = (T) new Object(); //commenting out because method is unsafe
+		this.horizontal = i;
+		this.vertical = i;
+	}
+		
 }
