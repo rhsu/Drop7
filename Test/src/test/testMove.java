f5c023c3ba9999177fa9f0fa212b929fcaa62265
robert <rhsu123@gmail.com>
2013-04-11 22:27:07 -0400

    Committing for backup
    Research how an array list works for add
    After I retrieve my pieces, they are still in order. How does that work?

--- Test/src/test/testMove.java
+++ Test/src/test/testMove.java
@@ -10,7 +10,7 @@ public class testMove
 	public static void main(String... args)
 	{
 		Board b = StaticBoards.getEmptyBoard();
-		b.pieceAt(5, 2).setValue(5);
+		/*b.pieceAt(5, 2).setValue(5);
 		
 		Piece p = b.pieceAt(5, 2);
 		
@@ -18,6 +18,22 @@ public class testMove
 		
 		b.move(p, BoardBase.Direction.DOWN);
 		
+		System.out.println(b);*/
+		
+		b.pieceAt(0, 2).setValue(1);
+		b.pieceAt(1, 2).setValue(2);
+		b.pieceAt(2, 2).setValue(3);
+		b.pieceAt(3, 2).setValue(4);
+		b.pieceAt(4, 2).setValue(5);
+		b.pieceAt(5, 2).setValue(6);
+		
 		System.out.println(b);
+		
+		System.out.println(b.getAllInColumn(b.pieceAt(4,2)));
+		
+		for(Piece p : b.getAllInColumn(b.pieceAt(4, 2)))
+		{
+			System.out.println(p);
+		}
 	}
 }
